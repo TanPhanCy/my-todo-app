79da1c41cdd83b9f4f13273a0f1822acd2e9797f
TanPhanCy <phan.tan.brussels@gmail.com>
2017-11-14 14:04:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>todo_app</title>
+
+  <!--MY CSS  -->
+  <link rel="stylesheet" style="text/css" href="style.css">
+
+  <!--MY FONTS  -->
+  <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+
+  <!--DATEPICKER PLUGIN  -->
+  <link rel="stylesheet" href="datepicker/dist/mtr-datepicker.min.css">
+  <link rel="stylesheet" href="datepicker/dist/mtr-datepicker.clutterboard-theme.min.css">
+</head>
+
+<body>
+
+  <div​ class="main">
+
+    <div class="main-header">
+
+      <img src="assets/img/plus.svg" alt="button">
+
+      <h1>MY TODOLIST</h1>
+      <hr width="99%" align="center"/>
+
+    </div>
+
+    <div​ class="main.container">
+
+    <ul class="list" id="done">
+
+      <li class="list-item">
+
+        <div class="item-inner" >
+
+          <div class="title"><span>Take a shower<span></div>
+            <div class="content"></div>
+            <div class="infos"></div>
+            <ul class="menuaction"></ul>
+
+        </div>
+      </li>
+
+      <li class="list-item">
+
+        <div class="item-inner" >
+
+          <div class="title"><span>Make my bag<span></div>
+            <div class="content"></div>
+            <div class="infos"></div>
+            <ul class="menuaction"></ul>
+
+        </div>
+      </li>
+
+      <li class="list-item">
+
+        <div class="item-inner" >
+
+          <div class="title"><span>Take a breakfast<span></div>
+            <div class="content"></div>
+            <div class="infos"></div>
+            <ul class="menuaction"></ul>
+
+        </div>
+      </li>
+
+    </ul>
+
+    <ul class="list" id="doing">
+      <li class="list-item">
+
+      <div class="item-inner">
+
+        <div class="title"><span>Go to bus stop</span></div>
+            <div class="content">My bus arrives at 7:30 I'll be at the station at 7:15</div>
+            <div class="infos">Started on: 2/10/2017 - 12:00 End Time: 3/10/2017 - 15:00</div>
+            <ul class="menuaction">
+              <li><a href="#">Delete</a></li>
+              <li><a href="#">Edit</a></li>
+            </ul>
+
+      </div>
+    </ul>
+
+    <hr width="99%" align="center" />
+
+    <ul class="list" id="todo">
+
+      <li class="list-item">
+
+        <div class="item-inner" >
+
+          <div class="title"><span>Be at BeCode on 9:00<span></div>
+            <div class="content"></div>
+            <div class="infos"></div>
+            <ul class="menuaction"></ul>
+
+        </div>
+      </li>
+
+      <li class="list-item">
+
+        <div class="item-inner" >
+
+          <div class="title"><span>Start Coding<span></div>
+            <div class="content"></div>
+            <div class="infos"></div>
+            <ul class="menuaction"></ul>
+
+        </div>
+      </li>
+
+      <li class="list-item">
+
+        <div class="item-inner" >
+
+          <div class="title"><span>I need a BREAK<span></div>
+            <div class="content"></div>
+            <div class="infos"></div>
+            <ul class="menuaction"></ul>
+
+        </div>
+      </li>
+
+    </ul>
+
+    <ul class="list" id="late">
+
+      <li class="list-item">
+
+        <div class="item-inner">
+
+          <div class="title"><span>Go to bus stop</span></div>
+          <div class="content"></div>
+          <div class="infos"></div>
+          <ul class="menuaction"></ul>
+
+        </div>
+
+      </li>
+    </ul>
+  </div>
+
+    <div class="add-container">
+
+    <a id="clear" href="#">Clear</a>
+
+    <form id="add">
+
+      <div class="row">
+        <label for="add-title">Title</label><br>
+        <input type="text" name="add-title" id="add-title" required placeholder="Title">
+      </div>
+      <hr width="99%" align="center"/>
+
+      <div class="row">
+        <label for="add-description">DESCRIPTION</label><br>
+        <input type="text" name="add-description" id="add-description" required placeholder="My todo description">
+      </div>
+      <hr width="99%" align="center"/>
+
+      <div class="row">
+        <label for="add-start-at">START-AT</label><br>
+        <input type="text" name="add-start-at" id="add-start-at" required placeholder="December 12, 2 PM">
+      </div>
+      <hr width="99%" align="center"/>
+
+      <div class="row">
+        <label for="add-end-at">END-AT</label><br>
+        <div id="demo"></div>
+      </div>
+      <hr width="99%" align="center" />
+
+      <ul class="list" id="add-new">
+
+        <li><input type="submit" value="Add and create new one"></li>
+        <li><input type="submit" value="Add task"></li>
+
+      </ul>
+      <hr width="99%" align="center" />
+
+    </form>
+
+  </div>
+
+    <div class="main-footer">
+
+    <ul class="list" id="display">
+
+        <li><a href="#">All task</a></li>
+        <li><a href="#">Todo task</a></li>
+        <li><a href="#">Done task</a></li>
+
+    </ul>
+
+  </div>
+</div>
+<script type="text/javascript" src="datepicker/dist/mtr-datepicker.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
